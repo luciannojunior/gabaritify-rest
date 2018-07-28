1ef9a6fc6124a0138e7930987a417c2f15be0da8
matheusgmaia <matheus.maia@ccc.ufcg.edu.br>
2018-07-28 15:15:05 -0300

    Adapt find answers to the new database

--- test_grader.py
+++ test_grader.py
@@ -9,120 +9,66 @@ import argparse
 import imutils
 import cv2
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to the input image")
-args = vars(ap.parse_args())
+def getFilteredContours(image):
+	#convert it to grayscale, blur it
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	
+	# apply Otsu's thresholding method to binarize the warped
+	# piece of paper
+	thresh = cv2.threshold(gray, 0, 255,
+		cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 
-# define the answer key which maps the question number
-# to the correct answer
-ANSWER_KEY = {0: 1, 1: 4, 2: 0, 3: 3, 4: 1}
+	#Aply mophology
+	kernel = np.ones((7,7),np.uint8)
+	thresh = cv2.dilate(thresh,kernel,iterations = 1)
 
-# load the image, convert it to grayscale, blur it
-# slightly, then find edges
-image = cv2.imread(args["image"])
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-edged = cv2.Canny(blurred, 75, 200)
+	# find contours in the thresholded image, then initialize
+	# the list of contours that correspond to questions
+	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+	questionCnts = []
 
-
-'''
-# find contours in the edge map, then initialize
-# the contour that corresponds to the document
-cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-docCnt = None
-
-# ensure that at least one contour was found
-if len(cnts) > 0:
-	# sort the contours according to their size in
-	# descending order
-	cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-
-	# loop over the sorted contours
+	# image with all contours
+	imageAll = image.copy()
+	#image with all questions contours
+	imageFilter = image.copy()
+	# loop over the contours
 	for c in cnts:
-		# approximate the contour
-		peri = cv2.arcLength(c, True)
-		approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+		# compute the bounding box of the contour, then use the
+		# bounding box to derive the aspect ratio
+		(x, y, w, h) = cv2.boundingRect(c)
+		ar = w / float(h)
+		cv2.rectangle(imageAll,(x,y),(x+w,y+h),(0,255,0),2)
 
-		# if our approximated contour has four points,
-		# then we can assume we have found the paper
-		if len(approx) == 4:
-			docCnt = approx
-			break
+		# in order to label the contour as a question, region
+		# should be sufficiently wide, sufficiently tall, and
+		# have an aspect ratio approximately equal to 1
+		area = w * h
+		if area > 7000 and ar >= 1.7 and ar <= 2.3:
+			questionCnts.append(c)
+			cv2.rectangle(imageFilter,(x,y),(x+w,y+h),(0,255,0),2)
+	cv2.imshow("ImagemFilter", imageFilter)
+	cv2.imshow("ImagemAll", imageAll)
 
-# apply a four point perspective transform to both the
-# original image and grayscale image to obtain a top-down
-# birds eye view of the paper
-paper = four_point_transform(image, docCnt.reshape(4, 2))
-warped = four_point_transform(gray, docCnt.reshape(4, 2))
-'''
-# apply Otsu's thresholding method to binarize the warped
-# piece of paper
-thresh = cv2.threshold(gray, 0, 255,
-	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 
-cv2.imshow("TreshAntes", thresh)
-kernel = np.ones((7,7),np.uint8)
-thresh = cv2.dilate(thresh,kernel,iterations = 1)
-cv2.imshow("TreshDepois", thresh)
+	# sort the question contours top-to-bottom, then initialize
+	# the total number of correct answers
+	questionCnts = contours.sort_contours(questionCnts,
+		method="top-to-bottom")[0]
 
-# find contours in the thresholded image, then initialize
-# the list of contours that correspond to questions
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-questionCnts = []
-
-imageAll = image.copy()
-imageFilter = image.copy()
-
-# loop over the contours
-for c in cnts:
-	# compute the bounding box of the contour, then use the
-	# bounding box to derive the aspect ratio
-	(x, y, w, h) = cv2.boundingRect(c)
-	ar = w / float(h)
-	cv2.rectangle(imageAll,(x,y),(x+w,y+h),(0,255,0),2)
-
-	# in order to label the contour as a question, region
-	# should be sufficiently wide, sufficiently tall, and
-	# have an aspect ratio approximately equal to 1
-	area = w * h
-	if area > 7000 and ar >= 1.7 and ar <= 2.3:
-		questionCnts.append(c)
-		cv2.rectangle(imageFilter,(x,y),(x+w,y+h),(0,255,0),2)
-cv2.imshow("ImagemAll", imageAll)
-cv2.imshow("ImagemFilter", imageFilter)
-
-# sort the question contours top-to-bottom, then initialize
-# the total number of correct answers
-questionCnts = contours.sort_contours(questionCnts,
-	method="top-to-bottom")[0]
-correct = 0
+	return (questionCnts, thresh)
 
 
 
-
-# each question has 5 possible answers, to loop over the
-# question in batches of 5
-for (q, i) in enumerate(np.arange(0, len(questionCnts), 5)):
-	# sort the contours for the current question from
-	# left to right, then initialize the index of the
-	# bubbled answer
-	cnts = contours.sort_contours(questionCnts[i:i + 5])[0]
+def correcAnswerInRow(cnts, thresh):
 	bubbled = None
-
 	# loop over the sorted contours
 	for (j, c) in enumerate(cnts):
 		# construct a mask that reveals only the current
 		# "bubble" for the question
 		mask = np.zeros(thresh.shape, dtype="uint8")
-
-		
-
+		cv2.drawContours(mask, [c], -1, 255, -1)
 		# apply the mask to the thresholded image, then
 		# count the number of non-zero pixels in the
 		# bubble area
@@ -132,27 +78,43 @@ for (q, i) in enumerate(np.arange(0, len(questionCnts), 5)):
 		# if the current total has a larger number of total
 		# non-zero pixels, then we are examining the currently
 		# bubbled-in answer
+		print(total, j)
 		if bubbled is None or total > bubbled[0]:
 			bubbled = (total, j)
+	return bubbled
 
-	# initialize the contour color and the index of the
-	# *correct* answer
-	color = (0, 0, 255)
-	k = 0#ANSWER_KEY[q]
+def answersFromContours(questionCnts, thresh):
+	answers = []
+	for (q, i) in enumerate(np.arange(0, len(questionCnts)-10, 4)):
+		# sort the contours for the current question from
+		# left to right, then initialize the index of the
+		# bubbled answer
+		cnts = contours.sort_contours(questionCnts[i:i + 4])[0]
+		bubbled = correcAnswerInRow(cnts, thresh)
+		# check to see if the bubbled answer is correct
+		answers.append(bubbled[1])
 
-	# check to see if the bubbled answer is correct
-	if k == bubbled[1]:
-		color = (0, 255, 0)
-		correct += 1
+	for (q, i) in enumerate(np.arange(len(questionCnts)-10, len(questionCnts), 2)):
+		# sort the contours for the current question from
+		# left to right, then initialize the index of the
+		# bubbled answer
+		cnts = contours.sort_contours(questionCnts[i:i + 2])[0]
+		bubbled = correcAnswerInRow(cnts, thresh)
+		# check to see if the bubbled answer is correct
+		answers.append(bubbled[1])
+	return answers
 
-	# draw the outline of the correct answer on the test
-	cv2.drawContours(blurred, [cnts[k]], -1, color, 3)
 
-# grab the test taker
-score = (correct / 5.0) * 100
-print("[INFO] score: {:.2f}%".format(score))
-cv2.putText(blurred, "{:.2f}%".format(score), (10, 30),
-	cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
-cv2.imshow("Original", image)
-cv2.imshow("Exam", blurred)
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to the input image")
+args = vars(ap.parse_args())
+
+# load the image, 
+# slightly, then find edges
+image = cv2.imread(args["image"])
+questionCnts, thresh = getFilteredContours(image)
+answers = answersFromContours(questionCnts, thresh)
+print(answers)
 cv2.waitKey(-1)
